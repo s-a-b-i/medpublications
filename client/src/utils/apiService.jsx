91b9ad67d088c93a5585756a18e5225173cc775e
Sohaib <21ntucs1360@gmail.com>
2026-02-19 01:35:59 +0500

    fix api service

--- client/src/utils/apiService.jsx
+++ client/src/utils/apiService.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 const API_URL = '/api';
 const PROTECTED_API_URL = '/api/protected';
 
+// Create axios instance WITH credentials for all requests
 const axiosInstance = axios.create({
   withCredentials: true
 });
@@ -24,48 +25,57 @@ export const logout = async () => {
   }
 };
 
+// Use axiosInstance for ALL protected routes
 export const fetchPublications = async () => {
-  const response = await axios.get(`${PROTECTED_API_URL}/publications`);
+  const response = await axiosInstance.get(`${PROTECTED_API_URL}/publications`);
   return response.data;
 };
 
 export const addPublication = async (publicationData) => {
-  const response = await axios.post(`${PROTECTED_API_URL}/publications`, publicationData, {
-    'Content-Type': 'multipart/form-data'
+  const response = await axiosInstance.post(`${PROTECTED_API_URL}/publications`, publicationData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
   });
   return response.data;
 };
 
 export const updatePublication = async (publicationData) => {
-  const response = await axios.put(`${PROTECTED_API_URL}/publications/${publicationData.get('_id')}`, publicationData, {
-    'Content-Type': 'multipart/form-data'
+  const response = await axiosInstance.put(`${PROTECTED_API_URL}/publications/${publicationData.get('_id')}`, publicationData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
   });
   return response.data;
 };
 
 export const deletePublication = async (id) => {
-  await axios.delete(`${PROTECTED_API_URL}/publications/${id}`);
+  await axiosInstance.delete(`${PROTECTED_API_URL}/publications/${id}`);
 };
 
 export const fetchMembers = async () => {
-  const response = await axios.get(`${PROTECTED_API_URL}/members`);
+  const response = await axiosInstance.get(`${PROTECTED_API_URL}/members`);
   return response.data;
 };
 
 export const addMember = async (memberData) => {
-  const response = await axios.post(`${PROTECTED_API_URL}/members`, memberData, {
-    'Content-Type': 'multipart/form-data'
+  const response = await axiosInstance.post(`${PROTECTED_API_URL}/members`, memberData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
   });
   return response.data;
 };
 
 export const updateMember = async (memberData) => {
-  const response = await axios.put(`${PROTECTED_API_URL}/members/${memberData.get('_id')}`, memberData, {
-    'Content-Type': 'multipart/form-data'
+  const response = await axiosInstance.put(`${PROTECTED_API_URL}/members/${memberData.get('_id')}`, memberData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
   });
   return response.data;
 };
 
 export const deleteMember = async (id) => {
-  await axios.delete(`${PROTECTED_API_URL}/members/${id}`);
+  await axiosInstance.delete(`${PROTECTED_API_URL}/members/${id}`);
 };
