034ab7c4847538b8e6c74ce02cbfa87026837e59
Sohaib <21ntucs1360@gmail.com>
2026-02-19 01:14:35 +0500

    packages update

--- server/createAdmin.js
+++ server/createAdmin.js
@@ -7,52 +7,52 @@
 
 // // Connect to MongoDB
 // const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('MongoDB connected successfully');
-//   } catch (error) {
-//     console.error('MongoDB connection error:', error);
-//     process.exit(1);
-//   }
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log('MongoDB connected successfully');
+//     } catch (error) {
+//         console.error('MongoDB connection error:', error);
+//         process.exit(1);
+//     }
 // };
 
 // // Function to create admin
 // const createAdmin = async () => {
-//   try {
-//     const existingAdmin = await User.findOne({ role: 'admin' });
+//     try {
+//         const existingAdmin = await User.findOne({ role: 'admin' });
 
-//     if (existingAdmin) {
-//       console.log('Admin user already exists');
-//       process.exit();
+//         if (existingAdmin) {
+//             console.log('Admin user already exists');
+//             process.exit();
+//         }
+
+//         const username = process.env.ADMIN_USERNAME || 'husnain';
+//         const password = process.env.ADMIN_PASSWORD || 'husnA123';
+
+//         // Hash the password
+//         const salt = await bcrypt.genSalt(10);
+//         const hashedPassword = await bcrypt.hash(password, salt);
+
+//         const adminUser = new User({
+//             username,
+//             password: hashedPassword,
+//             role: 'admin',
+//         });
+
+//         await adminUser.save();
+//         console.log(`Admin user created with username: ${username}`);
+//         process.exit();
+//     } catch (error) {
+//         console.error('Error creating admin user:', error);
+//         process.exit(1);
 //     }
-
-//     const username = process.env.ADMIN_USERNAME || 'husnain';
-//     const password = process.env.ADMIN_PASSWORD || 'husnA123';
-
-//     // Hash the password
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, salt);
-
-//     const adminUser = new User({
-//       username,
-//       password: hashedPassword,
-//       role: 'admin',
-//     });
-
-//     await adminUser.save();
-//     console.log(`Admin user created with username: ${username}`);
-//     process.exit();
-//   } catch (error) {
-//     console.error('Error creating admin user:', error);
-//     process.exit(1);
-//   }
 // };
 
 // // Run the functions
 // (async () => {
-//   await connectDB();
-//   await createAdmin();
+//     await connectDB();
+//     await createAdmin();
 // })();
